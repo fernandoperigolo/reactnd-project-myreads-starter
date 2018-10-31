6fd5d4bd203e293cb5205e11dc4651662c51271d
Fernando Souza <fernandoperigolo@gmail.com>
2018-10-30 21:19:50 -0300

    Tratando resultados sem autor ou sem imagem

--- src/BookItem.js
+++ src/BookItem.js
@@ -3,17 +3,19 @@ import BookShelfChanger from './BookShelfChanger';
 
 class BookItem extends Component {
   render() {
+    const backgroundImage = this.props.book.imageLinks ? 'url(' + this.props.book.imageLinks.smallThumbnail + ')' : 'none';
+    const authors = this.props.book.authors ? this.props.book.authors.join(", ") : 'Unknown Author'
     return (
       <li>
         <div className="book">
           <div className="book-top">
-            <div className="book-cover" style={{ width: 128, height: 193, backgroundImage: 'url('+this.props.book.imageLinks.smallThumbnail+')' }}></div>
+            <div className="book-cover" style={{ width: 128, height: 193, backgroundImage: backgroundImage }}></div>
             <div className="book-shelf-changer">
               <BookShelfChanger currentShelf={this.props.book.shelf} onChangeShelfBook={this.props.onChangeShelfBook} book={this.props.book} />
             </div>
           </div>
           <div className="book-title">{this.props.book.title}</div>
-          <div className="book-authors">{this.props.book.authors.join(", ")}</div>
+          <div className="book-authors">{authors}</div>
         </div>
       </li>
     );
